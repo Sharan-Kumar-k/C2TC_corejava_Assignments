4dc0e0eefc1d4e4b70dc97aafabff038eadffa78
Sharan-Kumar-k <sharansharan7950@gmail.com>
2025-08-18 16:09:09 +0530

    Assignment 2 completed

---
+++ C2TC_corejava_Assignments/src/com/sharankumar/assignment2/student/Student.java
@@ -0,0 +1,16 @@
+package com.sharankumar.assignment2.student;
+	/**
+	 * Student class demonstrates a default constructor
+	 */
+	public class Student {
+
+	    /**
+	     * Default constructor
+	     * Prints a message when a Student object is created
+	     */
+	    public Student() {
+	        System.out.println("Student object is created");
+	    }
+	}
+
+
